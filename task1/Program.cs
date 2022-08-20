c224c04cb4a65da3301df00d3e01c80e88da07f3
Protiana <protiana@yandex.ru>
2022-08-20 23:01:47 +0300

    task 1 seminar 2

---
+++ task1/Program.cs
@@ -0,0 +1,39 @@
+﻿//  принять 3х значное число и вернуть вторую его цифру
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondNumber(int i)
+{
+    int SecondNumber = 0;
+    if (i >= -999 && i <= -100 || i>=100 && i<=999)
+    {
+        SecondNumber = (i / 10) % 10;
+        return SecondNumber;
+    }
+    else
+    {
+        SecondNumber = i;
+        return SecondNumber;
+    }
+}
+int b = FindSecondNumber(n);
+if (b == n)
+{
+    Console.WriteLine($"Число {n} не трехзначное. Введите трехзначное число.");
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+
+/* заполнение нового массива
+int[] FillArray(int[] array){
+    Random random = new Random();
+    for(int i = 0; i<array.Length; i++){
+        array[i] = random.Next(1,124);
+    }
+return array;
+}
+;*/
