4abbbe2749847aec6473e54f658afd2f2e1c95f9
Protiana <protiana@yandex.ru>
2022-08-22 09:35:24 +0300

    2nd task

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//  принять число, выдать третью цифру заданного числа или сообщить, что ее нет
+
+Console.WriteLine("Введите число");
+string n = Convert.ToString(Console.ReadLine());
+if (n.Length > 2){
+  Console.WriteLine(n[2]);
+}
+else{
+Console.WriteLine("Нет третьего числа");
+}
